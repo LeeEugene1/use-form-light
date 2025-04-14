a10da82828d64a0f984c7206cdb940d6fb6ae4d3
LeeEugene1 <uwm1004@gmail.com>
2025-04-14 20:17:23 +0900

    init: build 설정

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.cjs",
+      format: "cjs",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "tsconfig.json",
+      useTsconfigDeclarationDir: true,
+    }),
+    terser(),
+  ],
+  external: ["react"],
+};
