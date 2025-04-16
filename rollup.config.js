c1e59a1389e2b9b5f7df67a265a9b3d8ee737699
LeeEugene1 <uwm1004@gmail.com>
2025-04-16 23:14:41 +0900

    ts

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,39 @@ import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.js",
-      format: "esm",
-      sourcemap: true,
-    },
-    {
-      file: "dist/index.cjs",
-      format: "cjs",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: "tsconfig.json",
-      useTsconfigDeclarationDir: true,
-    }),
-    terser(),
-  ],
-  external: ["react"],
-};
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.cjs",
+        format: "cjs",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "tsconfig.json",
+        useTsconfigDeclarationDir: true,
+        declaration: true,
+        declarationDir: "dist/types",
+      }),
+      terser(),
+    ],
+    external: ["react"],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
