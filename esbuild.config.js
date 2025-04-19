35eb592923e342b142649041ac9ea4f410914217
이유진 <yjlee@fimasia.co.kr>
2025-04-19 17:02:00 +0900

    init: rollup to esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,22 @@
+import esbuild from 'esbuild';
+
+const buildOptions = {
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  platform: 'browser',
+  target: ['es2015'],
+  format: 'esm',
+  outfile: 'dist/index.mjs',
+  sourcemap: true,
+  external: ['react', 'react/jsx-runtime'],
+};
+
+// Build ESM
+await esbuild.build(buildOptions);
+
+// Build CJS
+await esbuild.build({
+  ...buildOptions,
+  format: 'cjs',
+  outfile: 'dist/index.cjs',
+}); 
